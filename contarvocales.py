e6d439145b166da5016b2172a40e98d4af8848b5
LucasCartofield <l.cartofield@alumno.um.edu.ar>
2024-04-14 22:50:57 -0300

    archivo contarvocales.py agregado

---
+++ contarvocales.py
@@ -0,0 +1,21 @@
+import unittest
+
+
+def contar_vocales(palabra):
+    vocales = ("a", "e", "i", "o", "u")
+    resultado = {}
+    for letra in palabra:
+        if letra in vocales:
+            # La letra es vocal
+            if letra in resultado.keys():
+                # Sumar valor a diccionario ya existente
+                resultado[letra] += 1
+            else:
+                # Agregar letra por primera vez
+                resultado[letra] = 1
+
+    return resultado
+
+if __name__ == '__main__':
+    unittest.main()
+
